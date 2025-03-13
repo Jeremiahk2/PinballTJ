b15e98ea854f381dafc1f1509d34a4d1c254166b
Jeremiahk2 <TheMainMoe@gmail.com>
2025-03-13 19:16:30 -0400

    Adjusted paddles. Added right paddle physics

--- src/Scenes/RPaddle/r_paddle.gd
+++ src/Scenes/RPaddle/r_paddle.gd
@@ -1,10 +1,19 @@
 extends RigidBody2D
 
+@onready var startingRotation = transform.get_rotation();
+var paddleVelocity = 8;
+
 func _ready():
-	pass;
+	print(startingRotation);
+	print(transform.get_rotation());
 
 func _physics_process(delta: float):
-	if Input.is_action_pressed("ui_right"):
-		angular_velocity = 2;
-	elif Input.is_action_pressed("ui_left"):
+	print(transform.get_rotation());
+	var pressed = false;
+	angular_velocity = -paddleVelocity;
+	if transform.get_rotation() <= startingRotation:
 		angular_velocity = 0;
+	if Input.is_action_pressed("ui_right"):
+		angular_velocity = paddleVelocity;
+		if transform.get_rotation() >= -startingRotation:
+			angular_velocity = 0;
