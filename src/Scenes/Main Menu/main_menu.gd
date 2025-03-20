8db4edc8fafeb189ef766e1e893d92192ecbd6e1
Jeremiahk2 <TheMainMoe@gmail.com>
2025-03-20 19:54:48 -0400

    Added exit button and fixed scaling

--- src/Scenes/Main Menu/main_menu.gd
+++ src/Scenes/Main Menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
 func _input(event: InputEvent):
-	if Input.is_anything_pressed():
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	elif Input.is_anything_pressed():
 		get_tree().change_scene_to_file("res://Scenes/Main/Main.tscn")
