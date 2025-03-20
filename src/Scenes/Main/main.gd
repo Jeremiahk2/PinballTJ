aa39d0e53db0730b8e59fc483ef5788e8bd17bc2
Trip <thkelley@ncsu.edu>
2025-03-20 07:58:25 -0400

    Updated map and added some new objects to the main scene. added code for basic scoring in the main script but doesnt work in game yet

--- src/Scenes/Main/main.gd
+++ src/Scenes/Main/main.gd
@@ -3,10 +3,19 @@ extends Node2D
 var score = 0
 var lives = 2
 
+var point_values = {
+	"SquareJewel": 100,
+	"HeartGem": 50,
+	"GoldBar": 10
+}
+
 var introDialogue = load("res://Dialogue/intro.dialogue")
 
 func _ready():
 	$LoseArea.connect("body_entered", Callable(self, "on_lose_area_body_entered"))
+	$ScoreArea1.connect("body_entered", Callable(self, "on_score_area_body_entered"))
+	# $ScoreArea2.connect("body_entered", Callable(self, "on_score_area_body_entered"))
+	# $ScoreArea3.connect("body_entered", Callable(self, "on_score_area_body_entered"))
 	if !State.intro_done:
 		State.intro_done = true;
 		DialogueManager.dialogue_ended.connect(startGame)
@@ -27,3 +36,13 @@ func on_lose_area_body_entered(body):
 			$BallSpawner.call_deferred("spawn")
 		else:
 			get_tree().change_scene_to_file.call_deferred(("res://Scenes/Main Menu/Main Menu.tscn"))
+
+func on_score_area_body_entered(body):
+	if body.name == "ball":
+		var parent_name = body.get_parent().name
+		if parent_name in point_values:
+			increment_score(point_values[parent_name])
+
+func increment_score(value):
+	score += value
+	print("score: ", score)
