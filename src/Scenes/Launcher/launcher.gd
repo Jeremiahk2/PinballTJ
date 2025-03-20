b9cbd9017b0dc7cd1e1a8001979893a311ee17e7
Jeremiah Knizley <TheMainMoe@gmail.com>
2025-03-20 13:40:42 -0400

    Partial fix to launcher issue

--- src/Scenes/Launcher/launcher.gd
+++ src/Scenes/Launcher/launcher.gd
@@ -4,13 +4,12 @@ extends RigidBody2D
 
 func _process(delta: float):
 	if Input.is_action_pressed("ui_down"):
-		print("Position y: ", position.y);
 		if position.y >= orig_pos.y + 60:
 			linear_velocity.y = 0;
 		else:
-			linear_velocity = Vector2(0, 50);
+			linear_velocity.y = 50;
 	else:
 		if position.y <= orig_pos.y:
 			linear_velocity.y = 0;
 		else:
-			linear_velocity.y = -3500;
+			linear_velocity.y = -1000;
